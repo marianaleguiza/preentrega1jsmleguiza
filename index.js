dadf028905e881577a2af4e192cca6c41b31ab96
marianaleguiza <mariana.leguiza@gmail.com>
2023-06-19 01:44:22 -0300

    primera entrega js

---
+++ index.js
@@ -0,0 +1,25 @@
+let sumaPrecios = 0;
+let cantidadCuotas = 3;
+let deseaContinuar;
+
+function calcular(sumaPrecios, cantidadCuotas) {
+    do {
+        let precio = parseFloat(prompt('Ingrese el precio del producto: '));
+    
+        if (precio >= 10000 && precio <= 19999) {
+            cantidadCuotas = 6;
+        } else if (precio >= 20000) {
+            cantidadCuotas = 12;
+        }
+        
+        let subtotal = precio / cantidadCuotas;
+        sumaPrecios += precio;
+    
+        deseaContinuar = prompt('¿Desea agregar otro producto? (SI/NO):').toUpperCase();
+        console.log('El precio en cuotas será de: $' + subtotal + ' cada cuota');
+    } while (deseaContinuar === 'SI');
+    
+    console.log('La suma total de los precios es: $' + sumaPrecios);
+}
+
+calcular(sumaPrecios, cantidadCuotas);
